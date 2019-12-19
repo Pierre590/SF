03ec691fefc01f6c01364d2eccfb91e87f51ded7
pierre <nanashi4781@gmail.com>
2019-12-19 16:37:51 +0100

    sf

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+    * @Route("/", name="home")
+    */
+    public function home()
+    {
+            $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+            return $this->render('blog/home.html.twig',[
+            'articles' => $articles
+        ]);
+    }
+
+}
